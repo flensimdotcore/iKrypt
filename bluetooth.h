ca5e33b2d16d8f771dac5e45251f9d7b01dc6de4
Ivan Maraev <62958741+flensimdotcore@users.noreply.github.com>
2024-05-20 22:59:16 +0300

    Update bluetooth.h

--- bluetooth.h
+++ bluetooth.h
@@ -7,9 +7,6 @@
 #include "rfid.h"
 #include <EEPROM.h>
 
-
-/* Defining Bluetooth module pins and more */
-
 /* Serial1 pins are Bluetooth ones */
 
 #define INPUT_BUFFER_SIZE 19
@@ -17,30 +14,33 @@ extern char input_buffer[INPUT_BUFFER_SIZE];
 extern int input_buffer_counter;
 extern bool INPUT_DATA_FLAG;
 
-#define ACTIVATION_INPUT_BUFFER_SIZE 20
-#define ACTIVATION_SUCCESS 'S' // SUCCESS
-#define ACTIVATION_FAILURE 'F' // FAILURE
+/* Commands from module to phone */
 
-/* Defines for commands codes */
+#define RFID_AUTH_OK "RFID_AUTH\t0x00\t"
+#define RFID_AUTH_FAIL "RFID_AUTH\t0x01\t"
+#define LOCAL_STATE_CHANGE_EMPTY "LOCAL_STATE_CHANGE\t0x00\t0\t"
+#define LOCAL_STATE_CHANGE_MONEY "LOCAL_STATE_CHANGE\t0x00\t1\t"
+#define LOCAL_STATE_CHANGE_JEWELRY "LOCAL_STATE_CHANGE\t0x00\t2\t"
+#define LOCAL_STATE_CHANGE_WATCHES "LOCAL_STATE_CHANGE\t0x00\t3\t"
+#define LOCAL_STATE_CHANGE_DOOR_CLOSED "LOCAL_STATE_CHANGE\t0x00\t4\t"
 
-#define OPEN 'O' // OPEN
-#define LEFT_CRATE 'L' // LEFT
-#define RIGHT_CRATE 'R' // RIGHT
-#define TOP_CRATE 'T' // TOP
-#define BOTTOM_CRATE 'B' // BOTTOM
-#define CLOSE 'C' // CLOSE
-#define EMPTY_CENTER 'E' // EMPTY
-#define LICENSE_ACTIVATION 'A' // ACTIVATION
+/* Commands from phone to module */
 
-/* Defines for timers */
+#define PING "PING"
+#define IDENTIFY "IDENTIFY"
+#define OPEN_DOOR "OPEN_DOOR"
+#define SWITCH_CRATE "SWITCH_CRATE"
+#define GET_STATE "GET_STATE"
+#define CLOSE_DOOR "CLOSE_DOOR"
 
-#define BENCHMARK_TEST_TIME 3600000 // 1 hour in milliseconds
+/* Status and error codes */
 
-/* Special defines */
-
-//#define DEBUG // All the functions as in RELEASE version except some values are shown in Serial monitor
-//#define TEST // Benchmark test for studying a device's behaviour in extreme situations
-//#define RELEASE // Final stable version of firmware for consumers
+#define STATUS_CODE_OK "0x00"
+#define STATUS_CODE_RFID_NOT_PASSED "0x01"
+#define STATUS_CODE_CRATE_ALREADY_IN_CENTER "0x02"
+#define STATUS_CODE_AUTH_CODE_NOT_VALID "0x03"
+#define STATUS_CODE_TRIAL_ALREADY_ACTIVE "0x04"
+#define STATUS_CODE_CANT_ACCESS_MODULE "0x05"
 
 extern bool TRIAL_PERIOD_FLAG;
 extern bool LICENSE_FLAG;
@@ -48,8 +48,16 @@ extern bool WORK_FLAG;
 
 extern uint8_t cycles_left;
 
-void license_activation();
 void ping();
+void ping_res();
+void identify_res(char *STATUS_CODE);
+void open_door_res(char *STATUS_CODE);
+void switch_crate_res(char *STATUS_CODE);
+void get_state_res(char *STATUS_CODE);
+void close_door_res(char *STATUS_CODE);
+
+void license_activation();
+void ping_Bluetooth();
 void parser();
 
 #endif /* BLUETOOTH_H */
