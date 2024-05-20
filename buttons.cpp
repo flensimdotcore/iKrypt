0acb46b5bd0e9e3c23b2eb84acfb79f4eb946278
Ivan Maraev <62958741+flensimdotcore@users.noreply.github.com>
2024-05-20 23:00:20 +0300

    Update buttons.cpp

--- buttons.cpp
+++ buttons.cpp
@@ -2,14 +2,12 @@
 
 bool BUTTON_FLAG = 0;
 
-Button main_button(MAIN_BUTTON, 50, false, false);
 Button left_button(LEFT_BUTTON, 50, false, false);
 Button right_button(RIGHT_BUTTON, 50, false, false);
 Button top_button(TOP_BUTTON, 50, false, false);
 Button bottom_button(BOTTOM_BUTTON, 50, false, false);
 
 void buttons_init() {
-  main_button.begin();
   left_button.begin();
   right_button.begin();
   top_button.begin();
@@ -17,25 +15,28 @@ void buttons_init() {
 }
 
 void buttons_check() {
-  main_button.read();
   left_button.read();
   right_button.read();
   top_button.read();
   bottom_button.read();
 
-  if (main_button.isPressed() && left_button.isPressed()) {
-    current_signal = LEFT_CRATE_SIGNAL;
-    BUTTON_FLAG = 1;
-  } else if (main_button.isPressed() && right_button.isPressed()) {
+  if (right_button.isPressed()) {
     current_signal = RIGHT_CRATE_SIGNAL;
     BUTTON_FLAG = 1;
-  } else if (main_button.isPressed() && top_button.isPressed()) {
+  } else if (top_button.isPressed()) {
     current_signal = TOP_CRATE_SIGNAL;
     BUTTON_FLAG = 1;
-  } else if (main_button.isPressed() && bottom_button.isPressed()) {
+  } else if (bottom_button.isPressed()) {
     current_signal = BOTTOM_CRATE_SIGNAL;
+    BUTTON_FLAG = 1;
+  } else if (left_button.isPressed()) {
+    current_signal = LEFT_CRATE_SIGNAL;
     BUTTON_FLAG = 1;
   } else {
     BUTTON_FLAG = 0;
+  }
+
+  if (BUTTON_FLAG) {
+    motors_movement();
   }
 }
