f7800607194d434441bd9c239eea48b274d7e7d6
Ivan Maraev <62958741+flensimdotcore@users.noreply.github.com>
2024-05-20 23:10:41 +0300

    Update rfid.h

--- rfid.h
+++ rfid.h
@@ -1,10 +1,22 @@
 #ifndef RFID_H
 #define RFID_H
 
+#include "Wiegand.h"
+#include "bluetooth.h"
+#include "fsm.h"
+
 /* Defining RFID module pins and more */
 
-/* Serial2 pins are RFID ones */
+#define RFID_WIEGAND_D0 3
+#define RFID_WIEGAND_D1 2
 
+#define LED_PIN 20
+#define RELAY_PIN 21
+
+extern bool RFID_VALID_CARD_FLAG;
+extern bool RFID_OFF_FLAG;
+
+void RFID_init();
 void RFID_parser();
 
 #endif /* RFID_H */
